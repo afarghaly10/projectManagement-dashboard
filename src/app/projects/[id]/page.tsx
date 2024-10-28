36d7038faa7eb3fcc916676ff09de12d8642b91a
afarghaly10 <sami.farghaly@gmail.com>
2024-10-28 15:55:38 -0400

    redux api calls

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
+import ProjectHeader from "@/app/projects/ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const page = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+  return (
+    <div>
+      {/* {MODAL NEW TASKS} */}
+      {/* <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} /> */}
+      {/* {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )} */}
+    </div>
+  );
+};
+
+export default page;
